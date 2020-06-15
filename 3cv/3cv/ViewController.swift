54a5f72b49c16da0ae23820c7554bb2838a8735e
zbuster05 <zakary.saya@gmail.com>
2020-06-14 20:47:44 -0700

    Added Xcode template project

---
+++ 3cv/3cv/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  3cv
+//
+//  Created by Zachary on 6/14/20.
+//  Copyright © 2020 Shabang Systems, LLC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
