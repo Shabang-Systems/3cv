e962a102f2c3cc395bc493e9804786eb47c58239
TheEnquirer <huxmarv@nuevaschool.org>
2020-07-06 17:07:10 -0700

    some comments + safety commit

--- 3cv/3cv/PasteboardManager.swift
+++ 3cv/3cv/PasteboardManager.swift
@@ -10,8 +10,7 @@ import Foundation
 import Cocoa
 
 class Clipboard {
-    var prevItem = ""
-    let filename = getDocumentsDirectory().appendingPathComponent("history.txt")
+    
     
     // Set string to clipboard
     let pasteboard = NSPasteboard.general
@@ -19,46 +18,38 @@ class Clipboard {
     func readBoard() {
 
     }
-//    var clear = ""
+    
+    
+//#########################################################################################
+//# FILE MANAGEMENT #
+//#########################################################################################
+    
+    var prevItem = ""
+    let filename = getDocumentsDirectory().appendingPathComponent("history.txt")
     func saveToFile(value: String){
-        if value == prevItem {
-            print("unchanged")
-        } else {
+        if value == prevItem { print("unchanged") } else {
             prevItem = value
             do {
-//              try clear.write(to: filename, atomically: true, encoding: String.Encoding.utf8)
                 let fileHandle = try FileHandle(forWritingTo: filename)
                 fileHandle.seekToEndOfFile()
                 fileHandle.write(value.data(using: .utf8)!)
                 fileHandle.closeFile()
                 readFile(value: value)
-            } catch {
-               print("ohnoes")
-            }
-            
+            } catch { print("ohnoes") }
         }
-        
     }
     
     func readFile(value: String){
-        //reading
         do {
             let history = try String(contentsOf: filename, encoding: .utf8)
             print(history)
-        } catch {/* error handling here */}
+        } catch { print("ohnoes, dos") }
     }
 }
 
-
-// file stuff
 func getDocumentsDirectory() -> URL {
-    // find all possible documents directories for this user
     let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-
-    // just send back the first one, which ought to be the only one
     return paths[0]
 }
 
-
-
-
+//#########################################################################################
